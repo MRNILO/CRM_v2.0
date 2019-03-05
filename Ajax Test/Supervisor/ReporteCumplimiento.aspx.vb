2a474595be14e89d00dd5423023728c4faf955fa
Jorge <j_luis_b@hotmail.com>
2019-03-05 16:45:37 -0600

    Se modifican los reportes de ccalidad y el reporte de cumplimiento

--- Ajax Test/Supervisor/ReporteCumplimiento.aspx.vb
+++ Ajax Test/Supervisor/ReporteCumplimiento.aspx.vb
@@ -2,6 +2,7 @@
 
 Public Class ReporteCumplimiento
     Inherits System.Web.UI.Page
+    Private GE_Funciones As New Funciones
 
     Private Const XLS As String = ".xlsx"
     Private Ruta As String = ConfigurationManager.ConnectionStrings("RutaXLS").ConnectionString
@@ -34,71 +35,26 @@ Public Class ReporteCumplimiento
     Protected Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
         Dim FechaInicial As Date = deFechaInicial.Text
         Dim FechaFinal As Date = deFechaFinal.Text
+        Dim DTS As DataSet = New DataSet
 
         If FechaInicial > FechaFinal Then
             FechaInicial = deFechaFinal.Text
             FechaFinal = deFechaInicial.Text
         End If
 
-        Dim ROWA As DataRow
-        Dim DTA As New DataTable
+        DTS = GE_Funciones.Obtener_ReporteCumplimiento(FechaInicial, FechaFinal)
 
-        DTA.Columns.AddRange({New DataColumn("Proyecto", GetType(String)), New DataColumn("Cantidad", GetType(Integer)),
-                              New DataColumn("Campana", GetType(String)), New DataColumn("TipoCampana", GetType(String))})
+        If (DTS.Tables("DTA_DatosCumplimiento").Rows.Count > 0) Then
+            ViewState("DatosCumplimiento") = DTS
 
-        Dim Proyectos = BL.Obtener_Proyectos()
+            GV_CumplimientoData.DataSource = DTS.Tables("DTA_DatosCumplimiento")
+            GV_CumplimientoData.DataBind()
+            btnExcel.Enabled = True
+        Else
+            lbl_mensaje.Text = MostrarError("No hay información que procesar.")
+            btnExcel.Enabled = False
+        End If
 
-        For i As Integer = 0 To Proyectos.Length - 1
-            Dim DatosCumplimiento = BL.Obtener_TotalesCumplimientoProyecto(Proyectos(i).Proyecto, FechaInicial, FechaFinal)
-
-            If DatosCumplimiento.Length > 0 Then
-                For j As Integer = 0 To DatosCumplimiento.Length - 1
-                    ROWA = DTA.NewRow
-                    ROWA("Proyecto") = Proyectos(i).Proyecto.Trim
-                    ROWA("Cantidad") = DatosCumplimiento(j).Cantidad
-                    ROWA("Campana") = DatosCumplimiento(j).Campana.Trim
-                    ROWA("TipoCampana") = DatosCumplimiento(j).TipoCampana.Trim
-
-                    DTA.Rows.Add(ROWA)
-                Next
-            End If
-        Next
-
-        Dim ROWB As DataRow
-        Dim DTB As New DataTable
-
-        DTB.Columns.AddRange({New DataColumn("Semana", GetType(Integer)), New DataColumn("Asesor", GetType(String)), New DataColumn("Cliente", GetType(String)), New DataColumn("Ranking", GetType(String)),
-                              New DataColumn("Prototipo", GetType(String)), New DataColumn("Fraccionamiento", GetType(String)), New DataColumn("FechaInicio", GetType(Date)),
-                              New DataColumn("Etapa", GetType(String)), New DataColumn("campanaNombre", GetType(String))})
-
-        For i As Integer = 0 To Proyectos.Length - 1
-            Dim DetalleCumplimiento = BL.Obtener_DetallesCumplimientoProyecto(Proyectos(i).Proyecto, FechaInicial, FechaFinal)
-
-            If DetalleCumplimiento.Length > 0 Then
-                For j As Integer = 0 To DetalleCumplimiento.Length - 1
-                    ROWB = DTB.NewRow
-                    ROWB("Semana") = DetalleCumplimiento(j).semana
-                    ROWB("Asesor") = DetalleCumplimiento(j).asesor
-                    ROWB("Cliente") = DetalleCumplimiento(j).cliente
-                    ROWB("Ranking") = DetalleCumplimiento(j).Ranking
-                    ROWB("Prototipo") = DetalleCumplimiento(j).prototipo
-                    ROWB("Fraccionamiento") = DetalleCumplimiento(j).fraccionamiento
-                    ROWB("FechaInicio") = DetalleCumplimiento(j).fechaInicio
-                    ROWB("Etapa") = DetalleCumplimiento(j).etapa
-                    ROWB("campanaNombre") = DetalleCumplimiento(j).nombreCampaña
-
-                    DTB.Rows.Add(ROWB)
-                Next
-            End If
-        Next
-
-        Dim DS As New DataSet
-        DS.Tables.AddRange({DTA, DTB})
-
-        ViewState("DatosCumplimiento") = DS
-
-        GV_CumplimientoData.DataSource = DTA
-        GV_CumplimientoData.DataBind()
     End Sub
 
     Protected Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
