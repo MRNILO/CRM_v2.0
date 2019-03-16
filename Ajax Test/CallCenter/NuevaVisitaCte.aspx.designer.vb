8eda9e39b46ae6f2e63c2561922233a35adad857
Jorge <j_luis_b@hotmail.com>
2019-03-15 18:10:36 -0600

    Se agregan pantallas  NuevaVisitaCte en callcenter caseta y prospectador

---
+++ Ajax Test/CallCenter/NuevaVisitaCte.aspx.designer.vb
@@ -0,0 +1,177 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class NuevaVisitaCteCallCenter
+    
+    '''<summary>
+    '''Control lbl_nombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_nombre As Global.System.Web.UI.WebControls.Literal
+    
+    '''<summary>
+    '''Control cmBoxProyecto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cmBoxProyecto As Global.DevExpress.Web.ASPxComboBox
+    
+    '''<summary>
+    '''Control cmBoxModelo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cmBoxModelo As Global.DevExpress.Web.ASPxComboBox
+    
+    '''<summary>
+    '''Control txBoxEsquemaFinanciero.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxEsquemaFinanciero As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control txBoxMedio.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxMedio As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control txBoxCual.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxCual As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control dtFechaVisita.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents dtFechaVisita As Global.DevExpress.Web.ASPxDateEdit
+    
+    '''<summary>
+    '''Control txBoxUsuario.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxUsuario As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control txBoxAsesor.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxAsesor As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control dtp_finicio.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents dtp_finicio As Global.DevExpress.Web.ASPxDateEdit
+    
+    '''<summary>
+    '''Control dtp_ffinal.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents dtp_ffinal As Global.DevExpress.Web.ASPxDateEdit
+    
+    '''<summary>
+    '''Control txBoxTipoCamapana.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txBoxTipoCamapana As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control cmBoxClasificacion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cmBoxClasificacion As Global.DevExpress.Web.ASPxComboBox
+    
+    '''<summary>
+    '''Control cmBoxMotivo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cmBoxMotivo As Global.DevExpress.Web.ASPxComboBox
+    
+    '''<summary>
+    '''Control cmBoxSubMotivo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cmBoxSubMotivo As Global.DevExpress.Web.ASPxComboBox
+    
+    '''<summary>
+    '''Control btnAsignaVisita.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnAsignaVisita As Global.DevExpress.Web.ASPxButton
+    
+    '''<summary>
+    '''Control grdViewVisitas.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents grdViewVisitas As Global.DevExpress.Web.ASPxGridView
+    
+    '''<summary>
+    '''Control lbl_mensaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_mensaje As Global.System.Web.UI.WebControls.Literal
+End Class
---
+++ Ajax Test/Caseta/NuevaVisitaCte.aspx.vb
@@ -0,0 +1,170 @@
+﻿Imports Ajax_Test.Funciones
+
+Public Class NuevaVisitaCteCaseta
+    Inherits System.Web.UI.Page
+
+    Dim Usuario As New Servicio.CUsuarios
+    Dim NivelSeccion As Integer = 1
+
+    Dim IDCliente As Integer = 0
+
+    Private GE_Funciones As New Funciones
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ValidaUsuario()
+
+        Try
+            IDCliente = Request.QueryString("idCliente")
+        Catch ex As Exception
+            IDCliente = 0
+        End Try
+
+        If Not IsPostBack() Then
+            UI()
+        End If
+    End Sub
+
+#Region "Metodos"
+    Private Sub UI()
+        Dim DatosCitas As DatosCita = GE_Funciones.Obtener_DatosCita(IDCliente)
+
+        With txBoxEsquemaFinanciero : .Enabled = False : .Text = DatosCitas.TipoCredito : End With
+        With txBoxMedio : .Enabled = False : .Text = DatosCitas.Origen : End With
+        With txBoxCual : .Enabled = False : .Text = DatosCitas.LugarContacto : End With
+        With txBoxUsuario : .Enabled = False : .Text = DatosCitas.Asesor : End With
+        With txBoxAsesor : .Enabled = False : .Text = DatosCitas.AsesorAsignado : End With
+        With txBoxTipoCamapana : .Enabled = False : .Text = DatosCitas.TipoCampana : End With
+        With dtFechaVisita : .Enabled = False : .Date = Now() : End With
+        With dtp_finicio : .Enabled = False : .Date = Now() : End With
+        With dtp_ffinal : .Enabled = False : .Date = Now.AddDays(30) : End With
+
+        Alimentar_ComboProyectos()
+        Alimentar_ComboModelos(cmBoxProyecto.SelectedItem.Value)
+
+        Alimentar_ComboClasificacion()
+        Alimentar_ComboMotivos(cmBoxClasificacion.SelectedItem.Value)
+        Alimentar_ComboSubmotivos(cmBoxClasificacion.SelectedItem.Value, cmBoxMotivo.SelectedItem.Value)
+    End Sub
+
+    Private Sub Alimentar_ComboClasificacion()
+        With cmBoxClasificacion
+            .DataSource = GE_Funciones.Obtener_Clasificacion()
+            .ValueField = "Ranking"
+            .TextField = "Ranking"
+            .DataBind()
+
+            .SelectedIndex = 0
+        End With
+    End Sub
+
+    Private Sub Alimentar_ComboMotivos(ByVal Clasificacion As String)
+        With cmBoxMotivo
+            .DataSource = GE_Funciones.Obtener_Motivos(Clasificacion)
+            .ValueField = "id_tipoImpedimento"
+            .TextField = "TipoImpedimento"
+            .DataBind()
+
+            .SelectedIndex = 0
+        End With
+    End Sub
+
+    Private Sub Alimentar_ComboSubmotivos(ByVal Clasificacion As String, ByVal IdMotivo As Integer)
+        With cmBoxSubMotivo
+            .DataSource = GE_Funciones.Obtener_Submotivos(Clasificacion, IdMotivo)
+            .ValueField = "id_impedimento"
+            .TextField = "impedimento"
+            .DataBind()
+
+            .SelectedIndex = 0
+        End With
+    End Sub
+
+    Private Sub Alimentar_ComboProyectos()
+        With cmBoxProyecto
+            .DataSource = GE_Funciones.Obtener_Proyectos()
+            .ValueField = "Proyecto"
+            .TextField = "Fraccionamiento"
+            .DataBind()
+
+            .SelectedIndex = 0
+        End With
+    End Sub
+
+    Private Sub Alimentar_ComboModelos(ByVal Proyecto As String)
+        With cmBoxModelo
+            .DataSource = GE_Funciones.Obtener_ModelosXProyecto(Proyecto)
+            .ValueField = "id_producto"
+            .TextField = "Modelo"
+            .DataBind()
+
+            .SelectedIndex = 0
+        End With
+    End Sub
+#End Region
+
+#Region "FuncionesUsuario"
+    Sub ValidaUsuario()
+        If Not IsNothing(Session("Usuario")) Then
+            Usuario = Session("Usuario")
+            If Usuario.Nivel >= NivelSeccion Then
+                If String.IsNullOrEmpty(Request.QueryString("ReturnUrl")) Then
+                    Session("Usuario") = Usuario
+                Else
+                    Session("Usuario") = Usuario
+                    RedirigirSegunNivel(Usuario.Nivel)
+                End If
+            Else
+                'No valido
+                Session("Usuario") = Usuario
+                RedirigirSegunNivel(Usuario.Nivel)
+                'lbl_error.Text = MostrarError("Usuario o/y contraseña equivocados")
+            End If
+        Else
+            Session.Clear()
+            Response.Redirect("/Account/LogOn.aspx")
+        End If
+    End Sub
+
+    Sub RedirigirSegunNivel(ByVal Nivel As Integer)
+        Select Case Nivel
+            Case 1
+                Response.Redirect("~/Usuario/InicioUsuario.aspx", False)
+            Case 2
+                Response.Redirect("~/Supervisor/InicioSupervisor.aspx", False)
+            Case 3
+                Response.Redirect("~/Administrativo/InicioAdmin.aspx", False)
+        End Select
+    End Sub
+#End Region
+
+#Region "Eventos"
+    Protected Sub cmBoxClasificacion_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmBoxClasificacion.SelectedIndexChanged
+        Alimentar_ComboMotivos(cmBoxClasificacion.SelectedItem.Value)
+    End Sub
+
+    Protected Sub cmBoxMotivo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmBoxMotivo.SelectedIndexChanged
+        Alimentar_ComboSubmotivos(cmBoxClasificacion.SelectedItem.Value, cmBoxMotivo.SelectedItem.Value)
+    End Sub
+
+    Protected Sub btnAsignaVisita_Click(sender As Object, e As EventArgs) Handles btnAsignaVisita.Click
+        Dim DatosCitas As DatosCita = GE_Funciones.Obtener_DatosCita(IDCliente)
+
+        With DatosCitas
+            If BL.Insertar_VisitasClientes(.IdCita, .IdCliente, .IdUsuario, .IdUsuarioAsignado, Usuario.id_usuario, .IdCampana, cmBoxSubMotivo.SelectedItem.Value, .TipoCredito,
+                    0, cmBoxClasificacion.SelectedItem.Value, .Origen, cmBoxProyecto.SelectedItem.Value, cmBoxModelo.SelectedItem.Value,
+                    .TipoCampana, dtp_finicio.Text, dtp_ffinal.Text, dtFechaVisita.Text, 1) Then
+
+                Session("DatosCitas") = Nothing
+                lbl_mensaje.Text = MostrarExito("¡Visita registrada correctamente!")
+            Else
+                Session("DatosCitas") = Nothing
+                lbl_mensaje.Text = MostrarError("¡Ocurrio un error al registrar la visita!")
+            End If
+        End With
+    End Sub
+
+    Protected Sub cmBoxProyecto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmBoxProyecto.SelectedIndexChanged
+        Alimentar_ComboModelos(cmBoxProyecto.SelectedItem.Value)
+    End Sub
+#End Region
+End Class
